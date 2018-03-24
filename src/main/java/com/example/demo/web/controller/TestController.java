9a8c4661303b5e1438fe33022c5d3c940df6b0d5
vicky <435906052@qq.com>
2018-03-24 10:03:22 +0800

    test提交

---
+++ src/main/java/com/example/demo/web/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HuRui
+ * create on 2018/3/24 9:41
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test() {
+        return "Hello World!!!";
+    }
+}
