30553d9c9d6a6e1bafdd67b5176f6ed609501a39
vicky <435906052@qq.com>
2018-03-24 15:50:43 +0800

    test提交4444444444444444test

--- src/main/java/com/example/demo/web/controller/TestController.java
+++ src/main/java/com/example/demo/web/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     public String test() {
-        return "Hello World!!! staunch man 4444444444444444444444";
+        return "Hello World!!! staunch man 4444444444444444444444 test";
     }
 }
